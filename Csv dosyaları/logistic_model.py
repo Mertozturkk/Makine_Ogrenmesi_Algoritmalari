cd23b6989347581868d119eba65a2d827cc8f2d5
Mert Öztürk <42963332+Mertozturkk@users.noreply.github.com>
2020-10-18 17:57:43 +0300

    Add files via upload

---
+++ Csv dosyaları/logistic_model.py
@@ -0,0 +1,63 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+class LogisticRegression:
+    def __init__(self, n_features, n_observations):
+        self.weights = np.zeros((n_features, 1))
+        self.n_features = n_features
+        self.n_observations = n_observations
+        self.loss = []
+        self.acc = []
+    
+    def magic_sigmoid(self, z):
+        return 1/(1 + np.exp(-z))
+
+    def forward_pass(self, X, w):
+        return self.magic_sigmoid(np.matmul(X, w))
+    
+    def classify_or_prediction(self, X, w):
+        return np.round(self.forward_pass(X, w))
+    
+    def compute_loss(self, X, Y, w):
+        y_hat = self.forward_pass(X, w)
+        return -np.average(Y * np.log(y_hat) + (1-Y)*np.log(1-y_hat))
+
+    def compute_gradient(self, X, Y, w):
+        return np.matmul(X.T, (self.forward_pass(X, w) - Y)) / self.n_observations
+
+    def evaluate(self, X, Y, w):
+        loss = self.compute_loss(X, Y, w)
+        correct_predictions = np.sum(self.classify_or_prediction(X, w) == Y)
+        accuracy = correct_predictions / X.shape[0]
+        return accuracy, loss
+    def fit(self, X_train, Y_train, X_test, Y_test, max_iter, learning_rate):
+        print("Training..")
+        p_loss = 0.0000
+        for i in range(max_iter):            
+            self.weights -= self.compute_gradient(X_train, Y_train, self.weights) * learning_rate
+            validation_acc, validation_loss = self.evaluate(X_test, Y_test, self.weights)
+            training_acc, training_loss = self.evaluate(X_train, Y_train, self.weights)
+            self.acc.append(validation_acc)
+            self.loss.append(validation_loss)
+            if abs(p_loss - training_loss) < 0.0001:
+                break
+            p_loss = training_loss
+            print("Iter : {} Validation Accuracy: {:.5f} Validation Loss: {:.5f}".format(i, validation_acc, validation_loss))
+        print("Training process has been completed!")
+    
+    def plot(self):
+        plt.figure(figsize=(12, 4))
+
+        plt.subplot(1, 2, 1)
+        plt.plot(range(len(self.loss)), self.acc, 'r-')
+        plt.ylim([0., 1.])
+        plt.ylabel('Validation Accuracy')
+        plt.xlabel('Iterations')
+        plt.title('Accuracy: {:.3f}'.format(self.acc[-1]))
+
+        plt.subplot(1, 2, 2)
+        plt.plot(range(len(self.loss)), self.loss, 'b-')
+        plt.ylabel('Loss')
+        plt.xlabel('Iterations')
+        plt.title('Loss: {:.3f}'.format(self.loss[-1]))
+        plt.show()
